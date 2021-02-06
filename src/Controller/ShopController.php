07c9c5f6fdb3a0b62c95ce4e1b3f8aff1a54fbb1
AhmedGhanmi <Amustaphag22@gmail.com>
2021-02-06 22:08:56 +0100

    added product

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
 
 class ShopController extends AbstractController
 {
     /**
-     * @Route("/shop", name="shop")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProductRepository $repo): Response
     {
+        $products = $repo->findAll();
         return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ShopController',
+            'products' => $products
         ]);
+    }
+
+    /**
+     * @Route("/new-product", name="make-product")
+     */
+    public function makeProduct(Request $request, EntityManagerInterface $manager) : Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$product->getId()){
+                $product->setDatePosted(new \Date());
+            }
+            $manager->persist($product);
+            $manager->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('shop/create.html.twig', [
+            'formProduct' => $form->createView()]);
     }
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datePosted;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getDatePosted(): ?\DateTimeInterface
+    {
+        return $this->datePosted;
+    }
+
+    public function setDatePosted(\DateTimeInterface $datePosted): self
+    {
+        $this->datePosted = $datePosted;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
