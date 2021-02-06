1eca611ce0957a2f6d62689f04f91a763db7aaee
AhmedGhanmi <Amustaphag22@gmail.com>
2021-02-06 19:39:30 +0100

    project creation commit

---
+++ init.bat
@@ -0,0 +1,8 @@
+symfony composer require symfony/maker-bundle --dev
+symfony composer require doctrine/annotations
+symfony composer require doctrine
+symfony composer require twig
+symfony composer require sensio/framework-extra-bundle
+symfony composer require symfony/form
+composer require symfony/validator doctrine/annotations
+symfony composer require symfony/security-bundle
